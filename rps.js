b558d035f2b0dc20104a46346bdbd8ee0553241b
psanderl <paulsanderlin04@gmail.com>
2024-07-23 00:24:12 -0400

    Adds basic js

--- rps.js
+++ rps.js
@@ -1,1 +1,5 @@
-console.log("Hello World")
+console.log("Welcome to Rock Paper Scissors!")
+console.log("You will play 5 games! Best out of 5 Wins! Good Luck")
+window.prompt("You will go first. Your choice? (Player choice goes here)")
+
+
